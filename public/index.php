137a788c80c09d38ce13bff52ac570deaf89c32d
reiterus <reiterus@yandex.ru>
2023-04-05 10:24:58 +0300

    json exception, code style

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the MicroApi engine based on Symfony components.
+ * (c) Pavel Vasin <reiterus@yandex.ru>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use MicroApi\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
---
+++ src/Subscriber/Exception.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the MicroApi engine based on Symfony components.
+ * (c) Pavel Vasin <reiterus@yandex.ru>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace MicroApi\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class Exception implements EventSubscriberInterface
+{
+    /**
+     * JSON response on the "kernel.exception" event.
+     */
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $response = new JsonResponse();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $code = $exception->getStatusCode();
+        } else {
+            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        $response->setStatusCode($code);
+
+        $answer = json_encode([
+            'code' => $code,
+            'message' => $exception->getMessage(),
+        ]);
+
+        $response->setJson($answer);
+        $event->setResponse($response);
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+}
